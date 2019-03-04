9658bd670da3365d8ddb92dd9ee285e1c0fb89b4
Alexkrsz <alexkrsz@gmail.com>
2019-03-04 17:43:01 -0500

    First Update

--- Factorial.java
+++ Factorial.java
@@ -4,6 +4,7 @@ public class Factorial {
 
 	public static int factorial (int N)
 	 {
+		//This is N factorial
 		if (N <= 1)
 			
 			return 1;
@@ -13,6 +14,7 @@ public class Factorial {
 		 return (N * factorial(N-1));
 	 }
 	public static void main(String[] args) {
+		// Factorial of 5
 		int n;
 		n = 5;
  		n = factorial(n);
