9839d2baa7df465161b9640b8c9b218a35086568
Alexkrsz <alexkrsz@gmail.com>
2019-03-04 17:22:36 -0500

    initial commit

---
+++ FactorialTest.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FactorialTest {
+
+	@Test
+	public void factorialTest() {
+		fail("Not yet implemented");
+	}
+
+}
